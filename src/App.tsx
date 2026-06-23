7c7db29c7f2f2256f55e17a935389cc02c184c7f
diegojaureguitalkoot <diego.jauregui@talkoot.com>
2026-06-23 18:46:47 -0400

    add auth (#5)
    Co-authored-by: Sean Wang <seanwang@talkoot.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
 import { AppLayout } from "@/components/AppLayout";
 import CriteriaPage from "./pages/CriteriaPage";
 import SuitesPage from "./pages/SuitesPage";
@@ -11,9 +11,19 @@ import GenerationsPage from "./pages/GenerationsPage";
 import MappingPage from "./pages/MappingPage";
 import CriterionDetailPage from "./pages/CriterionDetailPage";
 import DataPage from "./pages/DataPage";
+import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
+import { isAuthenticated } from "@/lib/auth";
 
 const queryClient = new QueryClient();
+
+const RequireAuth = () => {
+  const location = useLocation();
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+  return <Outlet />;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -22,15 +32,18 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<CriteriaPage />} />
-            <Route path="/criteria" element={<CriteriaPage />} />
-            <Route path="/suites" element={<SuitesPage />} />
-            <Route path="/evaluate" element={<EvaluatePage />} />
-            <Route path="/generations" element={<GenerationsPage />} />
-            <Route path="/mapping" element={<MappingPage />} />
-            <Route path="/data" element={<DataPage />} />
-            <Route path="/criteria/:id" element={<CriterionDetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<RequireAuth />}>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<CriteriaPage />} />
+              <Route path="/criteria" element={<CriteriaPage />} />
+              <Route path="/suites" element={<SuitesPage />} />
+              <Route path="/evaluate" element={<EvaluatePage />} />
+              <Route path="/generations" element={<GenerationsPage />} />
+              <Route path="/mapping" element={<MappingPage />} />
+              <Route path="/data" element={<DataPage />} />
+              <Route path="/criteria/:id" element={<CriterionDetailPage />} />
+            </Route>
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
